8e04ea7e4dd766535debec151a31d912a6b5b0b3
Block Doc <matthew.stammers@uhs.nhs.uk>
2022-12-10 23:12:42 +0000

    regression complete

--- kedro-tutorial/src/kedro_tutorial/pipelines/data_processing/pipeline.py
+++ kedro-tutorial/src/kedro_tutorial/pipelines/data_processing/pipeline.py
@@ -1,32 +1,41 @@
-"""
-This is a boilerplate pipeline 'data_processing'
-generated using Kedro 0.18.4
-"""
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
-from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import evaluate_model, split_data, train_model
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
+    pipeline_instance = pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
+                func=split_data,
+                inputs=["model_input_table", "params:model_options"],
+                outputs=["X_train", "X_test", "y_train", "y_test"],
+                name="split_data_node",
             ),
             node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
+                func=train_model,
+                inputs=["X_train", "y_train"],
+                outputs="regressor",
+                name="train_model_node",
             ),
             node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
+                func=evaluate_model,
+                inputs=["regressor", "X_test", "y_test"],
+                outputs=None,
+                name="evaluate_model_node",
             ),
         ]
     )
+    ds_pipeline_1 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="active_modelling_pipeline",
+    )
+    ds_pipeline_2 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="candidate_modelling_pipeline",
+    )
+
+    return ds_pipeline_1 + ds_pipeline_2
